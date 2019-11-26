6e7803b8a87b0ed2644547cb4fac1a63279f1885
Wanbli83470 <estival.t@hotmail.com>
2019-11-26 09:17:21 +0100

    Préparation déploiement

--- melodyoga/yoga_website/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Atelier, Client, Inscribe
-# Register your models here.
-admin.site.register(Atelier)
-admin.site.register(Client)
-admin.site.register(Inscribe)
--- melodyoga/yoga_website/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class YogaWebsiteConfig(AppConfig):
-    name = 'yoga_website'

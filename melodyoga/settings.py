0da4bff29bde46644c126cf48a862e13ed7edda9
Wanbli83470 <estival.t@hotmail.com>
2019-11-27 09:26:40 +0100

    tentative heroku 4

--- melodyoga/settings.py
+++ melodyoga/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -141,16 +141,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = False
 
-    # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
     DEBUG = True
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'P8/static'),
+)

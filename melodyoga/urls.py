4ab2ee6eb6ea2238078048f4cf1f59b9dba8f6c1
Wanbli83470 <estival.t@hotmail.com>
2019-11-27 10:18:24 +0100

    correction bug 2 test

--- melodyoga/urls.py
+++ melodyoga/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('yoga_website.urls')),
+    path('/', include('yoga_website.urls')),
 ]

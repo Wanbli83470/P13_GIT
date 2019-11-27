d5cc192e565c23cd5ac1cb00566fd591b3a09b39
Wanbli83470 <estival.t@hotmail.com>
2019-11-27 10:01:18 +0100

    tentative 6

--- melodyoga/settings.py
+++ melodyoga/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -145,5 +145,5 @@ else:
     DEBUG = True
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'yoga_website/static'),
 )

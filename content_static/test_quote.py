6ada63bee5c31f649311a73f3f5d0220d44a36c0
Wanbli83470 <estival.t@hotmail.com>
2020-10-25 21:01:09 +0100

    Test de quote en "pseudo-mock" à l'aide du module "freezegun" pour figer une date.

--- content_static/test_quote.py
+++ content_static/test_quote.py
@@ -1,14 +1,17 @@
 import unittest
-from .quote import *
-import datetime
+from quote import quotes, QuoteWeek
+from freezegun import freeze_time
 
+
+@freeze_time("2020-01-29")
 class TestFonctionGet(unittest.TestCase):
 
     def setUp(self):
         self.quote_test = QuoteWeek().get_quote_week()
+        print(self.quote_test)
 
     def test_get_quote(self):
-
+        self.assertEqual(self.quote_test, quotes[5])
 
 
 if __name__ == '__main__':

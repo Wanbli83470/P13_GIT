071bfc614c7fe26dd3cb40d68cd213dd75af8acb
Wanbli83470 <estival.t@hotmail.com>
2020-10-25 21:05:24 +0100

    Documentation de test_quote.py : OK

--- content_static/test_quote.py
+++ content_static/test_quote.py
@@ -3,14 +3,16 @@ from quote import quotes, QuoteWeek
 from freezegun import freeze_time
 
 
-@freeze_time("2020-01-29")
+@freeze_time("2020-01-29")  # Simulation of the 5th week of the year
 class TestFonctionGet(unittest.TestCase):
 
     def setUp(self):
+        """Launch of the QuoteWeek module to obtain the result of its method"""
         self.quote_test = QuoteWeek().get_quote_week()
         print(self.quote_test)
 
     def test_get_quote(self):
+        """Comparison between the theoretical result and the practical result"""
         self.assertEqual(self.quote_test, quotes[5])
 
 
